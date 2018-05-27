11680ccfa7ccd02e2067a0b22e55b7f06089835d
JunahW <1347006821@qq.com>
2018-05-27 13:56:29 +0800

    新增工具类，注意SessionProvider的方法不是静态的，需要使用时记得注入，其余的工具类的方法皆为静态方法

--- eleba/src/com/eleba/controller/UserController.java
+++ eleba/src/com/eleba/controller/UserController.java
@@ -1,26 +1,32 @@
 package com.eleba.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.eleba.service.UserService;
 
 @RequestMapping(value="/user")
 @Controller
 public class UserController {
 	
-	@Autowired
-	private UserService userService;
 	
 	@RequestMapping(value="/add")
 	public void addUser() {
-		userService.addUser();
 	}
 	
 	@RequestMapping(value="/index")
 	public String index() {
 		return "admin/index";
+	}
+	
+	@Value("${img.server}")
+	private String imgServer;
+	
+	@RequestMapping(value="/test")
+	public void test() {
+		System.out.println(imgServer);
+		
 	}
 
 }
